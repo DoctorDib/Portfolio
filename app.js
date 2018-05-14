6c82b7c99218ca486c2436f8aec678f3ccb36085
root <root@vps543549.ovh.net>
2018-05-14 15:49:49 +0200

    Boooop

---
+++ app.js
@@ -0,0 +1,57 @@
+//const App = require('./templates/main.jsx');
+
+const express = require('express');
+//const ReactEngine = require('express-react-engine');
+const bodyParser = require('body-parser');
+const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const flash = require('connect-flash');
+
+const dependencies = require('./dependencies');
+
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+const ejs = require('ejs');
+
+
+dependencies.resolve(function(home){
+    const app = SetupExpress();
+
+    function SetupExpress(){
+        const app = express();
+        const server = http.createServer(app);
+
+        server.listen(8080, function(){
+            console.log('Listening on port 8080');
+        });
+
+        configureExpress(app);
+
+        // Setup Router/Routing
+        const router = require('express-promise-router')();
+        // grabbing main template
+        // Grabbing content
+
+        home.setRouting(router);
+
+        app.use(router);
+    }
+
+    function configureExpress(app){
+        app.use(express.static('/client/public'));
+        app.use(cookieParser());
+
+        app.set('views', __dirname + '/client/public');
+        app.set('view engine', 'ejs');
+
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({extended: true}));
+        app.use(validator());
+
+        app.use(flash());
+    }
+});
+
--- routers/home.js
+++ routers/home.js
@@ -4,15 +4,10 @@ module.exports = function(){
     return {
         setRouting: function(router){
             router.get('/', this.Main);
-            router.get('/login', this.Login);
         },
 
         Main: function(req, res){
-            res.render('main.jsx', {title: 'Portfolio'})
+            res.render('index/index.ejs', {title: 'Portfolio'})
         },
-
-        Login: function(req, res){
-            res.render('login.jsx', {title: 'Login'})
-        }
     }
 };
